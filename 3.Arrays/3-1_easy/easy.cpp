a2e8dca24678a469ffe50e97b7362088cf70f3f2
Govind <govind12771277@gmail.com>
2024-08-17 02:09:07 +0530

    array

--- 3.Arrays/3-1_easy/easy.cpp
+++ 3.Arrays/3-1_easy/easy.cpp
@@ -72,49 +72,104 @@ vector<int> rotateArray(vectore<int> &arr)
         arr[i - 1] = arr[i];
     }
     arr[n - 1] = temp;
-    return arr ;
+    return arr;
 }
 
-
-// 6. left rotate array by n place 
+// 6. left rotate array by n place
 // method 1
-void rotateArrayN(vector<int> &arr , int d)
+void rotateArrayN(vector<int> &arr, int d)
 {
     int n = arr.size();
-    d = d % n ;
+    d = d % n;
 
     vector<int> temp[d];
-    for (int i=0 ; i<d ; i++){
+    for (int i = 0; i < d; i++)
+    {
         temp[i] = arr[i];
     }
 
-    for (int i=d ; i<n ; i++){
-        arr[i-d]  = arr[i]
+    for (int i = d; i < n; i++)
+    {
+        arr[i - d] = arr[i]
     }
 
-    for (int i=n-d ; i<n ; i++){
-        arr[i] = temp[i-(n-d)] ;
+    for (int i = n - d; i < n; i++)
+    {
+        arr[i] = temp[i - (n - d)];
     }
 
     // time complexity = O(n+d) => O(d)+ O(n-d) + O(d)
     // space complexity = O(d)
-
 }
 
-// method 2 (optimal) 
-void rotateArrayByN(vector<int> &arr , int d){
+// method 2 (optimal)
+void rotateArrayByN(vector<int> &arr, int d)
+{
     int n = arr.size();
-    d = d % n ;
+    d = d % n;
 
-    reverse(arr.begin() , arr.begin()+d);
-    reverse(arr.begin()+d , arr.begin()+n);
-    reverse(arr.begin() , arr.end());
+    reverse(arr.begin(), arr.begin() + d);
+    reverse(arr.begin() + d, arr.begin() + n);
+    reverse(arr.begin(), arr.end());
 
-    // time complexity = O(2n) 
-    // space complexity = O(1) 
+    // time complexity = O(2n)
+    // space complexity = O(1)
     // time comp. increased and space comp. decreased
 }
 
+// 7. move Zero to the end
+// brutte force solution
+void moveZero(vector<int> &arr)
+{
+    int n = arr.size();
+
+    vector<int> temp;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            temp.emplace_back(arr[i]);
+        }
+    }
+    int nz = temp.size();
+    for (int i = 0; i < nz; i++)
+    {
+        arr[i] = temp[i];
+    }
+    for (int i = nz; i < n; i++)
+    {
+        arr[i] = o;
+    }
+    // time : 2n
+    // space : N (worst);
+}
+
+// optimal
+void movezero(vector<int> &arr)
+{
+    int n = arr.size();
+    int j = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] == 0){
+            j = i ;
+            break;
+        }
+    }
+    if (j == -1 ) return ;
+
+    for (int i = j + 1; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+    // time : n
+    // space : 1
+
+}
 
 int main()
 {
@@ -122,7 +177,11 @@ int main()
     // slargest();
     // sorted();
     // removeDuplicates();
-    // rotateArray(); 
+    // rotateArray();
+
     // rotateArrayN();
     // rotateArrayByN();
+
+    moveZero();
+    movezero()
 }
