a1199f0714bce61b830056d5585aa67d04b5d010
Govind <govind12771277@gmail.com>
2026-05-24 13:37:19 +0530

    function finish

--- cohort/functions/5.cpp
+++ cohort/functions/5.cpp
@@ -1,11 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
 
+    // Check divisibility from 2 to n-1
+    for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0) return false;
+        }
+
+    return true;
+}
 
 int main() {
     int a ; 
     cin >> a; 
+
+    for (int i = 2; i <= a; i++)
+    {
+        if(isPrime(i)) cout << i << " ";
+    }
 
     return 0;
 }
