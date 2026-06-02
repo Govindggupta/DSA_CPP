1e6d4cff4ceb1e4e6be04fa824e11b824fbfb0aa
Govind <govind12771277@gmail.com>
2026-06-02 23:57:44 +0530

    time and space questions

--- cohort/time&spaceComplexity/countInterval.cpp
+++ cohort/time&spaceComplexity/countInterval.cpp
@@ -5,7 +5,28 @@ int main() {
     int q ; 
     cin >> q; 
 
-    while 
+    while (q > 0) {
+        int t;
+        long long l, r;
+        cin >> t >> l >> r;
+        
+        long long count = 0;
+        
+        if (l <= r) {
+            if (t == 1) {
+                count = r - l - 1;
+            } else if (t == 2) {
+                count = r - l;
+            } else if (t == 3) {
+                count = r - l;
+            } else { // t == 4
+                count = r - l + 1;
+            }
+        }
+        
+        cout << count << endl;
+        q--;
+    }
 
     return 0;
 }
